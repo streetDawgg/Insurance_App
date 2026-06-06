c3d3ce2582f00f54bdb437f7c70b68a6dd85c657
Ben Bartolazo <Bartolazobenezer@gmail.com>
2026-06-07 03:03:09 +0800

    Revised cars.js API(CRUD)

--- Insurance_Project/src/routes/cars.js
+++ Insurance_Project/src/routes/cars.js
@@ -30,6 +30,82 @@ router.get("/", async (req, res) =>{
     }
 
 });
+// patch
+
+router.patch("/custom/year/:make/:model", async (req, res) => {
+    const { make, model } = req.params;
+    const { year } = req.body || {};
+
+        if(!req.body){
+            return res.status(400).json({
+                message: "Body required"
+            });
+        }
+
+        if(isNaN(year)){
+            return res.status(400).json({
+                message: "Year must be numeric"
+            });
+        }
+
+        const vehicle = await Car.findOneAndUpdate({
+            make:new RegExp(`^${make}$`, "i"),
+            model:new RegExp(`^${model}$`, "i"),
+        },
+        {
+            year:Number(year)
+        },
+        {
+            returnDocument:"after"
+        }
+    );
+        if (!vehicle){
+            return res.status(404).json({message: "Vehicle not found"})
+        }
+        res.json({
+            message:"Year updated successfully",
+            updatedVehicle: vehicle
+        })
+});
+
+router.patch("/custom/engine/:make/:model", async (req, res) => {
+    const { make, model } = req.params;    
+    const { engine } = req.body || {};
+
+    if(!req.body){
+        return res.status(400).json({
+            message:"Body required"
+        });
+    }
+
+     if(!engine){
+            return res.status(400).json({
+                message: "Engine required"
+            });
+        }
+
+        const vehicle = await Car.findOneAndUpdate({
+                make: make,
+                model: model
+        },
+        {
+            engine:engine,
+        },
+        {
+            returnDocument:"after"
+        }
+        )
+
+        if (!vehicle){
+            return res.status(404).json({message: "Vehicle not found"})
+        }
+
+        res.json({
+            message:"Engine updated",
+            updatedVehicle: vehicle
+        })
+});
+
 
 //filkter type
 router.get("/filter/type/:type", async (req, res) => {
@@ -74,6 +150,20 @@ router.get("/filter/year/:year", async (req, res) =>{
     res.json(vehicle);
 });
 ////==============================================================================================================================
+
+//
+
+// dynamic ROUTES
+//get car or make car
+router.post("/", async (req, res) =>{
+    try{
+        const newCar = await Car.create(req.body);
+        res.status(201).json(newCar);
+    } catch (error){
+        res.status(500).json({message: error.message});
+    }
+});
+
 // Delete vehicle
 router.delete("/:make/:model", async (req, res) =>{
     const {make, model} = req.params
@@ -91,24 +181,12 @@ router.delete("/:make/:model", async (req, res) =>{
         deletedVehicle:vehicle
     });
 });
-//
-
-// dynamic ROUTES
-//get car
-router.post("/", async (req, res) =>{
-    try{
-        const newCar = await Car.create(req.body);
-        res.status(201).json(newCar);
-    } catch (error){
-        res.status(500).json({message: error.message});
-    }
-});
 
 // get make
 router.get("/:make", async (req,res) =>{
  const car = await Car.find({ make: new RegExp(`^${req.params.make}$`, "i") });;
 
-    if(!car){
+    if(car.length === 0){
         return res.status(404).json({message:"Make not found"})
     }
     res.json(car)
